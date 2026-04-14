a584e0e2fbc9aedee8fd919110bd8258ad1a7ecd
Jhalnath001 <jhalnath.chapagain17@gmail.com>
2026-04-14 07:41:01 +0545

    First commit

---
+++ hello.py
@@ -0,0 +1,57 @@
+# name="Jhalnath"
+# print("Hello "+name+" Whats up?")
+# p=input("Enter Principle:")
+# t=input("Enter Time:")
+# r=input("Enter Rate:")
+# si=(int(p)*int(t)*float(r))/100
+# print("SI=",si)
+# print(f"Simple Interest={si}")
+# # print("SI="+str(si));
+# a=12.5
+# b=int(a)
+# print(b)
+# c=a>b
+# print (c)
+#Control Statement
+#if else
+
+# if(a%2==0):
+#     print(f"{a} is Even Number!")
+# else:
+#     print(f"{a} is Odd Number!")
+#elif
+# if(a>0):
+#     print("Positive!")
+# elif(a<0):
+#     print("Negative")
+# else:
+#     print("Zero")
+#loop
+# for i in range(1,11):
+#     print(a,"*",i,"=",a*i)
+
+#print reverse of a number 
+# a=int(input("Enter a number:"))
+# rev=0
+# while a!=0:
+#     rem=a%10
+#     rev=rev*10+rem
+#     a//=10
+# print("Reverse of a number is:",rev)
+
+# i=1
+# while True:
+#     print(i,"\n")
+#     i+=1
+#     if i==100:
+#         break
+#array
+# arr=[10,20,50,40,30]
+# arr.sort()
+# arr.reverse();
+# print(arr)
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
